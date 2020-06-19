3b2a20530152a841b52e0b517c3b4af369432522
Max Ziebell <mail@maxziebell.de>
2020-06-19 20:19:02 +0200

    Update HypeHandlebars.js

--- HypeHandlebars.js
+++ HypeHandlebars.js
@@ -96,10 +96,6 @@ if("HypeHandlebars" in window === false) window['HypeHandlebars'] = (function ()
 
 		/**
 		 * This function is the what is actually being called to make the Handlebar updates and set the option defaults. These include addition helper functions and variable shortcuts. The Handlebars in Hype know about the context they are running in giving you access to hypeDocument and if available symbolInstance. This allow for function calls to the API of the hypeDocument or symbolInstance.
-		 * - disableHypeResolver: true removes the possibility to call Hype function from within Hype Handlebar, false is default
-		 * - disableQueryHelper: true disables the Handlebar helper for scene queries like $('.myElement') and document queries like querySelector('.test'), false is default
-		 * - disableHypeVariables: true disables Handlebar helper allowing shortcuts to current symbolInstance, symbolElement, hypeDocument, element and elementId, false is default
-		 * even more default Handlebar options can be found on the Handlebars documentation. Only listing Hype specific additions here
 		 *
 		 *     var elm = hypeDocument.getElementById('myElement');
  		 *     hypeDocument.updateHandlebarsByElement(elm, {
@@ -107,6 +103,11 @@ if("HypeHandlebars" in window === false) window['HypeHandlebars'] = (function ()
 		 *     	disableQueryHelper: true,
 		 *     	disableHypeVariables: true,
 		 *     });
+		 *
+		 * - disableHypeResolver: true removes the possibility to call Hype function from within Hype Handlebar, false is default
+		 * - disableQueryHelper: true disables the Handlebar helper for scene queries like $('.myElement') and document queries like querySelector('.test'), false is default
+		 * - disableHypeVariables: true disables Handlebar helper allowing shortcuts to current symbolInstance, symbolElement, hypeDocument, element and elementId, false is default
+		 * - even more default Handlebar options can be found on the Handlebars documentation. Only listing Hype specific additions here
 		 *
 		 * @param {Nodelist} elms is an optional nodelist to limit the update to a specific set of HTML elements
 		 * @param {Object} options is optional and allows for the usual Handlebar settings, defaults to useful settings for Hype if not set
